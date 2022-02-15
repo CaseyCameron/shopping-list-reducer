a0ed565a3cbd567faa5b565e3333038e70efd64b
CaseyCameron <mcasey.cameron@gmail.com>
2022-02-15 14:16:42 -0800

    Setting up Editing

--- src/components/ItemList/Item.jsx
+++ src/components/ItemList/Item.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Item({ item, onEdit, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  let editContent;
+
+  if (isEditing) {
+    editContent = (
+      <>
+        <input value={item.text} onChange={(e) => e.target.value} />
+        <button onClick={() => setIsEditing(false)}>Save</button>
+      </>
+    );
+  } else {
+    editContent = (
+      <>
+        <button type="button" onClick={() => setIsEditing(true)}>
+          Edit
+        </button>
+      </>
+    );
+  }
 
   return (
     <>
       <div>
         {item.text}
         <input type="checkbox" />
-        
+        {editContent}
         <button type="button" onClick={() => onDelete(item.id)}>
           Delete
         </button>
